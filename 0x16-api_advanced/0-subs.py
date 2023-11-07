ae1ae142da651ae761e23c470f0e3c43e7c633e5
HichamZineb <hichamelamine@hotmail.com>
2023-11-07 07:05:44 -0800

    newfile 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 0. How many subs? """
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number
+    of total subscribers for a given subreddit
+    """
+    import requests
+    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "UserAgent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code != 200:
+        return 0
+
+    return response.json().get("data").get("subscribers")
