dd8be8a16cb45a2ea97ce2bef67324b402f5cf50
HichamZineb <hichamelamine@hotmail.com>
2023-11-07 08:47:00 -0800

    update 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,7 +4,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], count=0, after=None):
     """
     Queries the Reddit API and returns a list containing the titles
     of all hot articles for a given subreddit.
@@ -22,10 +22,11 @@ def recurse(subreddit, hot_list=[], after=None):
     data = response.json().get("data")
     children = data.get("children")
     after = data.get("after")
+    count = data.get("count")
 
     hot_list += [child.get("data").get("title") for child in children]
 
     if not after:
         return hot_list
 
-    return recurse(subreddit, hot_list, after)
+    return recurse(subreddit, hot_list, count, after)
