36f187f324a02e6b68aef29982ef142c74b929ee
HichamZineb <hichamelamine@hotmail.com>
2023-11-07 07:08:41 -0800

    update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ def number_of_subscribers(subreddit):
     of total subscribers for a given subreddit
     """
     import requests
-    
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "UserAgent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         return 0
 
